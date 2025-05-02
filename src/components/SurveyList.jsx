27f8b2442d52532c6519b2e5c0994c4df8ffe6c3
RYBNetWork <constancechronicles@gmail.com>
2025-05-01 20:19:49 -0400

    Update SurveyList.jsx
    feat(survey): render animated list of survey responses with framer-motion transitions in SurveyList component
    
    Signed-off-by: RYBNetWork <constancechronicles@gmail.com>

--- src/components/SurveyList.jsx
+++ src/components/SurveyList.jsx
@@ -1,46 +1,31 @@
-import {motion, AnimatePresence} from 'framer-motion'
-import {useContext} from 'react'
-import SurveyItem from './SurveyItem'
-import SurveyContext from '../Context/SurveyContext'
+import { motion, AnimatePresence } from "framer-motion";
+import { useContext } from "react";
+import SurveyItem from "./SurveyItem";
+import SurveyContext from "../Context/SurveyContext";
 
 function SurveyList() {
-	const {survey} = useContext(SurveyContext)
+  const { survey } = useContext(SurveyContext);
 
- if (!survey || survey.length === 0) {
- 	return <p>No Response Reported Yet </p>
- }
+  if (!survey || survey.length === 0) {
+    return <p>No Response Reported Yet </p>;
+  }
 
- return ( 
-	<div className="survey-list">
-		<AnimatePresence>
-		{survey.map((item) => (
-		<motion.div 
-			key={item.id}
-			initial={{opacity: 0}}
-			animate={{opacity: 1}}
-			exit={{opacity: 0}}
-			>
-			<SurveyItem 
-				key={item.id} 
-				item={item} 
-			 />
-		</motion.div>
-	))}
-	</AnimatePresence>
-</div>
-
-)
-	// return ( 
-	// 	<div className="survey-list">
-	// 		{survey.map((item) => (
-	// 	<SurveyItem 
-	// 		key={item.id} 
-	// 		item={item} 
-	// 		surveyDelete={surveyDelete} />
-	// 	))}
-	// </div>
-
-	// )
+  return (
+    <div className="survey-list">
+      <AnimatePresence>
+        {survey.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <SurveyItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
 }
 
-export default SurveyList
+export default SurveyList;
